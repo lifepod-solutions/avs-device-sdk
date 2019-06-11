687f85e906f253568705d81480845c9db929794c
Girish Nanda <girishnanda@gmail.com>
2019-06-11 11:12:34 -0700

    adjust wake word sensitivity

--- KWD/KWDProvider/src/KeywordDetectorProvider.cpp
+++ KWD/KWDProvider/src/KeywordDetectorProvider.cpp
@@ -23,7 +23,7 @@
 
 #ifdef KWD_KITTAI
 /// The sensitivity of the Kitt.ai engine.
-static const double KITT_AI_SENSITIVITY = 0.38;
+static const double KITT_AI_SENSITIVITY = 0.42;
 
 /// The audio amplifier level of the Kitt.ai engine.
 static const float KITT_AI_AUDIO_GAIN = 2.0;
